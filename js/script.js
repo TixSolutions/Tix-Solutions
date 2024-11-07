b808bac1ac33f380ab72134c151132e358d63b08
TixSolutions <tix.incorporation@gmail.com>
2024-11-07 23:47:32 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Future JavaScript code can be added here
+    console.log('TIX Solutions website loaded successfully.');
+});
+
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1000, // Animation duration
+        once: true, // Whether animation should happen only once
+    });
+    console.log('TIX Solutions website loaded successfully.');
+});
